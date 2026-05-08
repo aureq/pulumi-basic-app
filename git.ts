c3513e724fdfa811a408bc740056d184ed7925b0
Aurelien Requiem <aurelien@requiem.fr>
2026-05-08 10:29:30 +1000

    🚀 integrate with pulum cloud deployment settings

---
+++ git.ts
@@ -0,0 +1,53 @@
+import * as fs from 'node:fs';
+
+/**
+ * `extractGitRemoteUrl()` extracts the remote URL for a given Git remote name.
+ *
+ * @param configFilePath The path where to find the git repository configuration file.
+ * @param remoteName The Git remote name to look for.
+ * @returns a string with the remote url, or null if the remote couldn't be found.
+ */
+export function extractGitRemoteUrl(remoteName: string, configFilePath?: string): string | null {
+
+    if (!configFilePath) {
+        configFilePath = "./.git/config";
+    }
+
+    try {
+        // Read the .git/config file content
+        const configContent = fs.readFileSync(configFilePath, 'utf8');
+
+        // Split the content into lines
+        const lines = configContent.split('\n');
+
+        let isInTargetRemote = false;
+        for (const line of lines) {
+            // Match the remote block header
+            const remoteHeaderMatch = line.match(/^\[remote \"(.+?)\"\]$/);
+
+            if (remoteHeaderMatch) {
+                const currentRemoteName = remoteHeaderMatch[1];
+                isInTargetRemote = currentRemoteName === remoteName;
+                continue;
+            }
+
+            // If inside the target remote block, look for the URL
+            if (isInTargetRemote) {
+                const urlMatch = line.match(/^\s*url\s*=\s*(.+)$/);
+                if (urlMatch) {
+                    return urlMatch[1].trim();
+                }
+            }
+        }
+
+        // If not found, return null
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
+
+export function extractGitRepository(remoteUrl: string): { username: string; repoName: string } | null {
+    const match = remoteUrl.match(/(?:github\.com[:\/])([^\/]+)\/([^\/]+)\.git$/);
+    return match ? { username: match[1], repoName: match[2] } : null;
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as pulumiDeploymentSettings from "./components/pulumi/deployment-settings";
+
+const appConfig = new pulumi.Config();
+
+export = async () => {
+
+    const deploymentSettings = new pulumiDeploymentSettings.PulumiDeploymentSettings("deploymentSettings", {
+        prefixName: "pulumi-basic-app",
+        accessToken: appConfig.get("pulumiAccessToken"),
+    });
+
+    return {};
+}
